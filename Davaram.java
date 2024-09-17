de5c20102d774c82589580e59604d0c1473fc2b3
DavramJavier <Harukasensei25@gmail.com>
2024-09-17 16:58:37 +0800

    java 2D array

---
+++ Davaram.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package davaram;
+
+/**
+ *
+ * @author kentr
+ */
+public class Davaram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int [] [] javier = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        {10,11,12}
+    };
+    
+   // int [] javier = {1,2,3};
+    
+   /*  for (int i=0;i<javier.length;i++){
+         for (int j=0;j<javier[i].length;j++){
+             System.out.print(javier[i][j]+" ");
+         };
+         System.out.println(javier);
+         };*/
+         System.out.print(javier[2][1]+ " ");
+     }
+    
+
+}
